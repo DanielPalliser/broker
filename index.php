a51829df276ca80a91825da9f531d2d3ffb215a2
DanielPalliser <danielpalliser@gmail.com>
2013-12-06 18:18:53 +0000

    Added index page

--- index.php
+++ index.php
@@ -7,16 +7,23 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/mystyle.css">
         <title></title>
     </head>
     <body>
-        <div>
-            
-            <div class="btn-group btn-group-lg">
-                <button type="button" action="new_user.php" class="btn btn-default">Sign In</button>
-                <button type="button" class="btn btn-default">Register</button>
+        <div class='container'>
+            <div class='panel panel-primary'>
+                <div class='panel-heading'>Insurance broker webapp</div>
+                Welcome to the insurance broker website! <br/> Please log in or register to continue.
+                <div class='panel-body'>
+                    <div class="btn-group btn-group-lg">
+                        <button type="button" class="btn btn-primary">Sign In</button>
+                        <button type="button" onclick="window.location.href='new_user.php'" class="btn btn-info">Register</button>
+                    </div>
+                </div>  
             </div>
-        </div>     
+        </div>
         <?php
         // put your code here
         ?>
